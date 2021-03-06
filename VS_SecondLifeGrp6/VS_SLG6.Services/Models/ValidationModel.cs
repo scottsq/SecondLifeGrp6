9bbbc2ee99b81262ed1620ccc00aaf792a69fbfc
RUHLMANN Olivier <ruhlmann.olivier@hotmail.fr>
2021-03-06 17:12:45 +0100

    [UPDATE] Completed user add validation

--- VS_SecondLifeGrp6/VS_SLG6.Services/Models/ValidationModel.cs
+++ VS_SecondLifeGrp6/VS_SLG6.Services/Models/ValidationModel.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace VS_SLG6.Services.Models
 {
-    public class ValidationModel<T> where T : class
+    public class ValidationModel<T> where T : notnull
     {
         public T Value { get; set; }
         public List<string> Errors { get; set; } = new List<string>();
--- VS_SecondLifeGrp6/VS_SLG6.Services/Validators/IValidator.cs
+++ VS_SecondLifeGrp6/VS_SLG6.Services/Validators/IValidator.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using VS_SLG6.Services.Models;
 
 namespace VS_SLG6.Services.Validators
 {
     public interface IValidator<T> where T : class
     {
-        bool canAdd(T obj);
-        bool canEdit(T obj);
-        bool canDelete(T obj);
+        public ValidationModel<bool> CanAdd(T obj);
+        public ValidationModel<bool> CanEdit(T obj);
+        public ValidationModel<bool> CanDelete(T obj);
     }
 }
