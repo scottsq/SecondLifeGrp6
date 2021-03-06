544daa58001a916f48c982c7a78238805d3325bd
RUHLMANN Olivier <ruhlmann.olivier@hotmail.fr>
2021-03-06 11:12:35 +0100

    [UPDATE] Added tests for user service

--- VS_SecondLifeGrp6/Services.Tester/UserCrudTester.cs
+++ VS_SecondLifeGrp6/Services.Tester/UserCrudTester.cs
@@ -14,15 +14,22 @@ namespace Services.Tester
     [TestClass]
     public class UserCrudTester : GenericCrudTester<User>
     {
+        private const string TooLongString = "aaabbbcccdddeeefffggghhhiiijjjkkk";
+        private const string BlankString = "     ";
+        private User user = new User();
+
         public UserCrudTester()
         {
+            user.Login = "John";
+            user.Password = "Smith";
+            user.Email = "john.smith@lecnam.net";
             var u = new User();
             u.Id = 0;
             u.Login = "test";
             var u2 = new User();
             u2.Id = 1;
             u2.Login = "test2";
-            InitBehavior(u, u2);
+            InitBehavior(u, u2, user);
             InitTests();
         }
 
@@ -44,6 +51,70 @@ namespace Services.Tester
         }
 
         [TestMethod]
+        public void Add_WithEmptyLogin_ThenError()
+        {
+            user.Login = null;
+            var res = _service.Add(user);
+            Assert.AreNotEqual(0, res.Errors.Count);
+        }
+
+        [TestMethod]
+        public void Add_WithBlankLogin_ThenError()
+        {
+            user.Login = BlankString;
+            var res = _service.Add(user);
+            Assert.AreNotEqual(0, res.Errors.Count);
+        }
+
+        [TestMethod]
+        public void Add_WithTooLongLogin_ThenError()
+        {
+            user.Login = TooLongString;
+            var res = _service.Add(user);
+            Assert.AreNotEqual(0, res.Errors.Count);
+        }
+
+        [TestMethod]
+        public void Add_WithBlankPassword_ThenError()
+        {
+            user.Password = BlankString;
+            var res = _service.Add(user);
+            Assert.AreNotEqual(0, res.Errors.Count);
+        }
+
+        [TestMethod]
+        public void Add_WithEmptyPassword_ThenError()
+        {
+            user.Password = null;
+            var res = _service.Add(user);
+            Assert.AreNotEqual(0, res.Errors.Count);
+        }
+
+        [TestMethod]
+        public void Add_WithTooLongPassword_ThenError()
+        {
+            user.Password = TooLongString;
+            var res = _service.Add(user);
+            Assert.AreNotEqual(0, res.Errors.Count);
+        }
+
+        [TestMethod]
+        public void Add_WithEmptydMail_ThenError()
+        {
+            user.Email = null;
+            var res = _service.Add(user);
+            Assert.AreNotEqual(0, res.Errors.Count);
+        }
+
+        [TestMethod]
+        public void Add_WithInvalidMail_ThenError()
+        {
+            user.Email = TooLongString;
+            var res = _service.Add(user);
+            Assert.AreNotEqual(0, res.Errors.Count);
+        }
+
+        [TestMethod]
         public void Patch_WithObject1Login_ThenObject1Login()
         {
             var o = new Operation<User>();
@@ -53,6 +124,21 @@ namespace Services.Tester
             var p = new JsonPatchDocument<User>(new List<Operation<User>> { o }, new DefaultContractResolver());
             _service.Patch(0, p);
             Assert.AreEqual(_defaultObjects[1].Login, _workingObjects[0].Login);
+        }
+
+        [TestMethod]
+        public void FindByMail_WithNull_ThenError()
+        {
+            var res = ((UserService)_service).FindByMail(null);
+            Assert.AreNotEqual(0, res.Errors.Count);
+        }
+
+        [TestMethod]
+        public void FindByMail_WithUser_ThenUser()
+        {
+            _service.Add(user);
+            var res = ((UserService)_service).FindByMail(user.Email);
+            Assert.AreEqual(user, res.Value);
         }
     }
 }
