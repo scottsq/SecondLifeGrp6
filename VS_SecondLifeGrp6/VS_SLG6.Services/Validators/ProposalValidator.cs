df151133ddf1fc3ca46f5b2d91dd5efb6455639d
RUHLMANN Olivier <ruhlmann.olivier@hotmail.fr>
2021-03-13 21:42:32 +0100

    [UPDATE] overrided canAdd method for RatingValidator

--- VS_SecondLifeGrp6/VS_SLG6.Services/Validators/ProposalValidator.cs
+++ VS_SecondLifeGrp6/VS_SLG6.Services/Validators/ProposalValidator.cs
@@ -34,11 +34,13 @@ namespace VS_SLG6.Services.Validators
                 return _validationModel;
             }
             // Check negative price
-            if (obj.Price < 0) _validationModel.Errors.Add("Proposal price cannot be negative.");
+            if (obj.Price < 0) _validationModel.Errors.Add("Proposal Price cannot be negative.");
             // Check if Origin exists
             if (_serviceUser.Get(obj.Origin.Id) == null) _validationModel.Errors.Add("Proposal Origin doesn't exist.");
             // Check if Target exists
             if (_serviceUser.Get(obj.Target.Id) == null) _validationModel.Errors.Add("Proposal Target doesn't exist.");
+            // Check if Product exists
+            if (_serviceProduct.Get(obj.Product.Id) == null) _validationModel.Errors.Add("Proposal Product doesn't exist.");
             // Init state as ACTIVE
             obj.State = State.ACTIVE;
             _validationModel.Value = _validationModel.Errors.Count == 0;
--- VS_SecondLifeGrp6/VS_SLG6.Services/Validators/RatingValidator.cs
+++ VS_SecondLifeGrp6/VS_SLG6.Services/Validators/RatingValidator.cs
@@ -18,5 +18,34 @@ namespace VS_SLG6.Services.Validators
             _serviceProduct = serviceProduct;
             _serviceUser = serviceUser;
         }
+
+        public override ValidationModel<bool> CanAdd(Rating obj)
+        {
+            _validationModel.Value = false;
+            if (obj == null)
+            {
+                _validationModel.Errors.Add("Cannot add a null rating");
+                return _validationModel;
+            }
+            // Check null fields
+            if (obj.User == null || obj.Product == null)
+            {
+                _validationModel.Errors.Add("Rating object cannot have empty fields.");
+                return _validationModel;
+            }
+            // Check stars between 1 and 5
+            if (obj.Stars < 1 || obj.Stars > 5) _validationModel.Errors.Add("Rating Stars must be between 1 and 5.");
+            // Check if User exists
+            if (_serviceUser.Get(obj.User.Id) == null) _validationModel.Errors.Add("Rating User doesn't exist.");
+            // Check if Product exists
+            if (_serviceProduct.Get(obj.Product.Id) == null) _validationModel.Errors.Add("Rating Product doesn't exist.");
+            // Check if Rating already exists
+            if (_repo.FindAll(x => x.Product.Id == obj.Product.Id && x.User.Id == obj.User.Id).Count > 0)
+            {
+                _validationModel.Errors.Add("Rating on this Product already exists for this User.");
+            }
+            _validationModel.Value = _validationModel.Errors.Count == 0;
+            return _validationModel;
+        }
     }
 }
