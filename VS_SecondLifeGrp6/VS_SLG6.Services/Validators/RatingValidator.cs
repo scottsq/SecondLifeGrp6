8c7b460aea737b5abf06dae790fe2b42ce1c4657
RUHLMANN Olivier <ruhlmann.olivier@hotmail.fr>
2021-03-13 21:34:15 +0100

    [ADD] Added test class for RatingService

--- VS_SecondLifeGrp6/VS_SLG6.Services/Validators/RatingValidator.cs
+++ VS_SecondLifeGrp6/VS_SLG6.Services/Validators/RatingValidator.cs
@@ -4,11 +4,19 @@ using System.Text;
 using VS_SLG6.Model.Entities;
 using VS_SLG6.Repositories.Repositories;
 using VS_SLG6.Services.Models;
+using VS_SLG6.Services.Services;
 
 namespace VS_SLG6.Services.Validators
 {
     public class RatingValidator : GenericValidator<Rating>, IValidator<Rating>
     {
-        public RatingValidator(IRepository<Rating> repo, ValidationModel<bool> validationModel) : base(repo, validationModel) { }
+        private IService<Product> _serviceProduct;
+        private IService<User> _serviceUser;
+
+        public RatingValidator(IRepository<Rating> repo, ValidationModel<bool> validationModel, IService<Product> serviceProduct, IService<User> serviceUser) : base(repo, validationModel) 
+        {
+            _serviceProduct = serviceProduct;
+            _serviceUser = serviceUser;
+        }
     }
 }
