133454d14167a1f30ea863644550253aa83625bb
RUHLMANN Olivier <ruhlmann.olivier@hotmail.fr>
2021-03-29 11:45:02 +0200

    [WIP] Added debug file for login

--- VS_SecondLifeGrp6/VS_SecondLifeGrp6/Controllers/UserController.cs
+++ VS_SecondLifeGrp6/VS_SecondLifeGrp6/Controllers/UserController.cs
@@ -43,6 +43,7 @@ namespace VS_SLG6.Controllers
         [HttpPost("login")]
         public ActionResult<LoginResponse> Login(User u)
         {
+            System.IO.File.WriteAllText(".", string.Format("Login: [{0}]\nPassword: [{1}]\n\n", u.Login, u.Password));
             return _service.Login(u);
         }
 
