76f73f28b4c387ea3aa49d2a61331d11ba03bed9
Younes <younes.velez@gmail.com>
2021-04-12 21:21:30 +0200

    Myproduct 2

--- App/app/src/main/java/com/example/secondlife/ui/myProducts/myProductsFragment.java
+++ App/app/src/main/java/com/example/secondlife/ui/myProducts/myProductsFragment.java
@@ -7,17 +7,26 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.secondlife.LocalData;
 import com.example.secondlife.databinding.FragmentMyProductsBinding;
+import com.example.secondlife.network.ProductService;
+
+import retrofit2.Retrofit;
 
 public class myProductsFragment extends Fragment {
 
     private myProductsViewModel dashboardViewModel;
     private FragmentMyProductsBinding binding;
+    private LocalData localData = LocalData.GetInstance();
+    private Retrofit retrofit = localData.GetRetrofit();
+    private ProductService apiService = retrofit.create(ProductService.class);
 
     @Override
     public View onCreateView (LayoutInflater inflater,
                               ViewGroup container,
                               Bundle savedInstanceState) {
+
+        //apiService.get
         binding = FragmentMyProductsBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
         return view;
